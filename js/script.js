e91dd1332994985c05021b4e475929bf4ed27d1e
istvian <e.r94@outlook.cl>
2021-09-22 00:53:34 -0300

    video de fondo y correcciones

--- js/script.js
+++ js/script.js
@@ -6,19 +6,42 @@ function myFunction() {
         x.style.display = "flex";
     }
 }
-var span = document.getElementsByClassName("close")[0];
 var modal = document.getElementById("myModal");
-span.onclick = function() {
-    modal.style.display = "none";
-}
 modal.onclick = function() {
-    modal.style.display = "none";
+        modal.style.display = "none";
+    }
+    /*
+    function pop(file, text) {
+        var img = document.getElementById("popImg");
+        var caption = document.getElementById("caption");
+        modal.style.display = "block";
+        img.src = 'img/photos/' + file + '.jpg';
+        caption.innerHTML = text;
+    }*/
+    // function pop(number) {
+    //     console.log(`item${number}`);
+    //     var item = document.getElementById(`item${number}`);
+    //     var img = document.getElementById("popImg");
+    //     var caption = document.getElementById("caption");
+    //     modal.style.display = "block";
+    //     console.log(item.children[0].children[0].getAttribute("src"));
+    //     // img.src = `${item.children[0].children.getAttribute("src")}`;
+    //     caption.innerHTML = item.parentElement.childNodes[1].innerHTML;
+    // }
+
+var result = Array.from(document.getElementsByClassName('gallery__item'));
+
+for (let index = 0; index < result.length; index++) {
+    result[index].addEventListener("click", function() {
+        show(index);
+    }, true);
 }
 
-function pop(file, text) {
+function show(id) {
     var img = document.getElementById("popImg");
     var caption = document.getElementById("caption");
-    modal.style.display = "block";
-    img.src = 'img/photos/' + file + '.jpg';
-    caption.innerHTML = text;
+    modal.style.display = "flex";
+    img.src = `${result[id].children[0].getAttribute("src")}`;
+    caption.innerHTML = result[id].children[1].innerHTML;
+
 }
