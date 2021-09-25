e5c74535e65d0011f8ceb15f7f62ae525014e3e7
istvian <e.r94@outlook.cl>
2021-09-25 16:53:58 -0300

    Agrego loading

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// BOTON HAMBURGER
 function myFunction() {
     var x = document.getElementById("hamb");
     if (x.style.display === "flex") {
@@ -6,37 +7,19 @@ function myFunction() {
         x.style.display = "flex";
     }
 }
+// MODAL POPUP DESAPARECER
 var modal = document.getElementById("myModal");
 modal.onclick = function() {
         modal.style.display = "none";
     }
-    /*
-    function pop(file, text) {
-        var img = document.getElementById("popImg");
-        var caption = document.getElementById("caption");
-        modal.style.display = "block";
-        img.src = 'img/photos/' + file + '.jpg';
-        caption.innerHTML = text;
-    }*/
-    // function pop(number) {
-    //     console.log(`item${number}`);
-    //     var item = document.getElementById(`item${number}`);
-    //     var img = document.getElementById("popImg");
-    //     var caption = document.getElementById("caption");
-    //     modal.style.display = "block";
-    //     console.log(item.children[0].children[0].getAttribute("src"));
-    //     // img.src = `${item.children[0].children.getAttribute("src")}`;
-    //     caption.innerHTML = item.parentElement.childNodes[1].innerHTML;
-    // }
-
+    // LISTADO DE ITEMS
 var result = Array.from(document.getElementsByClassName('gallery__item'));
-
 for (let index = 0; index < result.length; index++) {
     result[index].addEventListener("click", function() {
         show(index);
     }, true);
 }
-
+// MOSTRAR POPUP
 function show(id) {
     var img = document.getElementById("popImg");
     var caption = document.getElementById("caption");
@@ -44,3 +27,10 @@ function show(id) {
     img.src = `${result[id].children[0].getAttribute("src")}`;
     caption.innerHTML = result[id].children[1].innerHTML;
 }
+
+// LOADING
+let loading = document.querySelector(".loader");
+console.log(loading);
+window.addEventListener("load", function() {
+    loading.style.display = "none";
+})
